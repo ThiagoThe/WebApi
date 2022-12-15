34dabf4bfc2aee24b865c550f0adc205cf7f9c28
ThiagoThe <nitrox2thi@gmail.com>
2022-12-14 22:27:33 -0300

    Adicionado novas rotas no app.js

--- app.js
+++ app.js
@@ -31,6 +31,35 @@ router.get("/clientes/:id?", async function (req, res, next) {
     res.status(400).json({ erro: `${ex}` });
   }
 });
+
+//rota de adição
+router.post("/clientes", async function (req, res, next) {
+  try {
+    const customer = req.body;
+    const db = await connect();
+    res.json(await db.collection("customers").insertOne(customer));
+  } catch (ex) {
+    console.log(ex);
+    res.status(400).json({ erro: `${ex}` });
+  }
+});
+
+//rota de alteração
+router.put("/clientes/:id", async function (req, res, next) {
+  try {
+    const customer = req.body;
+    const db = await connect();
+    res.json(
+      await db
+        .collection("customers")
+        .updateOne({ _id: new ObjectId(req.params.id) }, { $set: customer })
+    );
+  } catch (ex) {
+    console.log(ex);
+    res.status(400).json({ erro: `${ex}` });
+  }
+});
+
 app.use("/", router);
 
 //inicia o servidor
