eb9e4506d1e7e46d125384b7d1594dea6c5a9a4d
ThiagoThe <nitrox2thi@gmail.com>
2022-12-15 22:13:42 -0300

    adicionado rota update e delete

--- app.js
+++ app.js
@@ -60,6 +60,36 @@ router.put("/clientes/:id", async function (req, res, next) {
   }
 });
 
+//rota de update de dados parcial
+router.patch("/clientes/:id", async function (req, res, next) {
+  try {
+    const customer = req.body;
+    const db = await connect();
+    const id = { _id: new ObjectId(req.params.id) };
+    res.json(
+      await db.collection("customers").updateOne(id, { $set: customer })
+    );
+  } catch (ex) {
+    console.log(ex);
+    res.status(400).json({ erro: `${ex}` });
+  }
+});
+
+//rota delete
+router.delete("/clientes/:id", async function (req, res, next) {
+  try {
+    const db = await connect();
+    res.json(
+      await db
+        .collection("customers")
+        .deleteOne({ _id: new ObjectId(req.params.id) })
+    );
+  } catch (ex) {
+    console.log(ex);
+    res.status(400).json({ erro: `${ex}` });
+  }
+});
+
 app.use("/", router);
 
 //inicia o servidor
